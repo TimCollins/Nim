6205a0d23348d23b1f9fbd4a2e1a7767fabc5f4d
Tim <tim@timcollins.ie>
2018-03-24 17:38:03 +0000

    Fixed version number.

--- src/UnitTests/run-report.bat
+++ src/UnitTests/run-report.bat
@@ -1,1 +1,1 @@
-..\..\..\packages\ReportGenerator.3.1.2\tools\reportgenerator.exe -reports:results.xml -targetdir:coverage
+..\..\..\packages\ReportGenerator.3.1.1\tools\reportgenerator.exe -reports:results.xml -targetdir:coverage
